76cf919ae22873a1d793c6bfe88ef1a468ef0087
gonzaloapp@gmail.com <gonzaloapp@gmail.com>
2022-05-15 03:58:23 +0200

    maquetado listado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './pages/movie/movie-list/movie-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movies', component: MovieListComponent,
+    children: [
+      
+    ]
+  },
+  { path: '**', redirectTo: '/movies', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MoviesListComponent } from './pages/movie-list/movie-list.component';
 import { MenuComponent } from './pages/menu/menu.component';
-import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
-import { MovieAddComponent } from './pages/movie-add/movie-add.component';
+import { MovieListComponent } from './pages/movie/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesListComponent,
     MenuComponent,
-    MovieDetailComponent,
-    MovieAddComponent
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
